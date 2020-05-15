7eeb984299c54160746de8a2c9ecaba4092df9dc
Thomas Schuehly <thomas.schuehly@outlook.com>
2020-05-15 10:15:37 +0200

    Spring Init

---
+++ src/main/java/de/cschilingtschuehly/gcwebx/GcwebxApplication.java
@@ -0,0 +1,13 @@
+package de.cschilingtschuehly.gcwebx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcwebxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcwebxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/cschilingtschuehly/gcwebx/GcwebxApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cschilingtschuehly.gcwebx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcwebxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
