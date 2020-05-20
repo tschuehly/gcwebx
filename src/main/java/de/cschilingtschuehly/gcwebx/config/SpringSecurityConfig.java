6dada4fad69aed854f0409c8830aabf33d15b2ac
Thomas Schuehly <thomas.schuehly@outlook.com>
2020-05-20 12:07:21 +0200

    Added SecurityConfig and filled Membermodel

---
+++ src/main/java/de/cschilingtschuehly/gcwebx/config/SpringSecurityConfig.java
@@ -0,0 +1,33 @@
+package de.cschilingtschuehly.gcwebx.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}user").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}admin").roles("USER","ADMIN");
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                //HTTP Basic authentication
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/getMember").hasRole("USER")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
+
+}
--- src/main/java/de/cschilingtschuehly/gcwebx/modell/Member.java
+++ src/main/java/de/cschilingtschuehly/gcwebx/modell/Member.java
@@ -1,4 +1,22 @@
 package de.cschilingtschuehly.gcwebx.modell;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+import javax.persistence.Entity;
+
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String teamspeakId;
+    private String generalInfo;
+    private Date dateOfBirth;
+    private String desblTeam;
+    private Date joinDate;
+    private Date acceptanceDate;
+    private String editor;
+    private Integer warnings;
 }
