cf2bdc200052069836e24d6375cd7195d823f4ce
Thomas Schuehly <thomas.schuehly@outlook.com>
2020-09-30 22:48:05 +0200

    teamselector working for mobile, nav items full width in mobile

--- gcwebxFrontend/src/app/components/navigation/navigation.component.ts
+++ gcwebxFrontend/src/app/components/navigation/navigation.component.ts
@@ -24,7 +24,8 @@ export class NavigationComponent implements OnInit {
   public currentRoles: Roles;
   public currentRoute: string;
   public showTsToast = false;
-public navbarCollapsed = true;
+  public navbarCollapsed = true;
+  public teamSelection = 'ngbDropdown';
   constructor(private router: Router, public authenticationService: AuthenticationService) {
     this.authenticationService.currentRoles$.subscribe( value => {
       this.currentRoles = value;
